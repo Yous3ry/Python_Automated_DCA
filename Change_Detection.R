ee3cb4c67297c1ba0a4f820ae4e0f4e1410c8a43
Yous3ry <mohamed.yousry.1989@gmail.com>
2022-04-28 11:56:18 +0200

    R Script Change detection
    Sample Wells and R Change Detection Script

---
+++ Change_Detection.R
@@ -0,0 +1,35 @@
+# load Sample well data
+df <- read.csv(file = 'Sample_Well.csv', header=TRUE)
+df$Date <-as.Date(df$Date, format = "%m/%d/%Y")
+df.index <- df$Date
+head(df)
+
+# convert data to time series
+oil_ts <- ts(data=df[,2], frequency=12)
+oil_ts
+
+# Change points library
+library(EnvCpt)
+
+# define models to be used to find Change points
+models <- c("trendar1cpt", "trendar2cpt")
+# Find Change points
+out <- envcpt(log10(oil_ts), verbose=0, models=models, minseglen=6) 
+# Note you can change Min length as needed
+AIC(out)
+plot(out, type="fit")
+# find minimum AIC model
+optimal_model <- which.min(AIC(out))
+optimal_model <- names(optimal_model[1])
+optimal_model
+# Find change points in model
+change_points <- cpts(out[[optimal_model]])
+change_points
+
+# plot results
+plot(df, log='y')
+grid(nx = NULL, ny = NULL,
+     lty = 2,      # Grid line type
+     col = "gray", # Grid line color
+     lwd = 1)      # Grid line width
+abline(v = df$Date[change_points], col="red", lwd=2)
